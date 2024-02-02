6a7b2e2c058bb1d1ea2a88e3b946b843edac6b19
thuan-nguyen <thuan-nguyen@cybozu.vn>
2024-02-03 00:11:36 +0700

    add methods for doubly linked list

--- LinkedList/DoublyLinkedList.java
+++ LinkedList/DoublyLinkedList.java
@@ -15,10 +15,69 @@ public class DoublyLinkedList {
     void insertAtHead(int data) {
         Node newNode = new Node(data);
         if (head == null) {
-            this.head = newNode;
-            this.tail = newNode;
-            return;
+            this.head = this.tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
         }
-        // add case if not empty list
+    }
+
+    void insertAtTail(int data) {
+        Node newNode = new Node(data);
+        if (tail == null) {
+            this.head = this.tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    void insertAtIndex(int data, int idx) {
+        Node newNode = new Node(data);
+        int tmpIdx = 0;
+        Node temp = this.head;
+        
+        while (temp.next != null && tmpIdx != idx){
+            temp = temp.next;
+            tmpIdx++;
+        }
+
+        newNode.prev = temp.prev;
+        newNode.next = temp;
+        temp.prev.next = newNode;
+        temp.prev = newNode;
+
+    }
+
+    void print() {
+        Node temp = this.head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    void printBackward() {
+        Node temp = this.tail;
+        while (temp != null) {
+            System.err.print(temp.data + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+        list.insertAtTail(3);
+        list.insertAtTail(4);
+        list.insertAtTail(5);
+        list.insertAtIndex(6, 2);
+        
+        list.printBackward();
     }
 }
