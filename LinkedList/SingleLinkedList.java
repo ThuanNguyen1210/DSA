556a4cc56a04423b18acc04d3c661714314e2ace
Nguyen Ngoc Thuan <thuan-nguyen@cybozu.vn>
2024-02-02 16:46:02 +0700

    single linked list

---
+++ LinkedList/SingleLinkedList.java
@@ -0,0 +1,66 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+class SingleLinkedList {
+    Node head;
+
+    void insertAtHead(int data) {
+        Node newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    void insertAtTail(int data) {
+        Node newNode = new Node(data);
+        if (this.head == null) {
+            this.head = newNode;
+            return;
+        }
+        Node temp = this.head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void insertAtIndex(int data, int idx) {
+        int tempIdx = 0;
+        Node prev = this.head;
+        Node cur = this.head;
+
+        while (cur != null && tempIdx != idx) {
+            prev = cur;
+            cur = cur.next;
+            tempIdx++;
+        }
+
+        Node newNode = new Node(data);
+        prev.next = newNode;
+        newNode.next = cur;
+    }
+
+    void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        SingleLinkedList list = new SingleLinkedList();
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+        list.insertAtTail(3);
+        list.insertAtIndex(4, 1);
+
+        list.print();
+    }
+}
